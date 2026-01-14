7649aff5e35701c2936b2b68ec67f0723ef68614
mdmahimkhan77250@gmail.com <your_email@example.com>
2026-01-15 03:20:14 +0600

    implement login from

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,20 @@
-console.log("File connected")
+document.getElementById('login-btn').addEventListener('click', function (event) {
+
+    event.preventDefault()
+
+    const accountNumber = document.getElementById('account-number').value;
+    const pinNumber = document.getElementById('pin').value;
+
+    if (accountNumber.length === 6) {
+        if (pinNumber === "1234") {
+            window.location.href = "./main.html"
+        }
+        else {
+            alert("wrong pin")
+        }
+    }
+    else {
+        alert("need valid number")
+    }
+
+})
